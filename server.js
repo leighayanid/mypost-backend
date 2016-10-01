b733560aff94807dc3683d72d9dc4bcb1c366fb4
leighdinaya <leighdinaya04@gmail.com>
2016-10-01 10:25:50 -0700

    Changed to mongoose driver instead of mongodb. Added function to retrieve all the messages

--- server.js
+++ server.js
@@ -1,30 +1,44 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var mongo = require('mongodb').MongoClient;
+var mongoose = require('mongoose');
 
-var database;
+var Message = mongoose.model('Message', {
+    msg: String
+}); //uppercase meaning this is something needs to instantiate
 
 app.use(bodyParser.json());
-app.use(function(req,res,next){
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
 });
 
-app.post('/api/message', function(req,res){
+//creat a post method
+app.post('/api/message', function (req, res) {
     console.log(req.body);
-     database.collection("messages").insertOne(req.body);
+    var message = new Message(req.body);
+    message.save();
     res.status(200);
 })
 
-mongo.connect('mongodb://localhost:27017/mypost', function(err, db){
-    if(!err){
+//function to retrieve all the post/messages
+function GetMessages(){
+    //passing empty param
+    Message.find({}).exec(function(err,result){
+        console.log(result);
+    });
+}
+
+//connect to mongodb
+mongoose.connect('mongodb://localhost:27017/mypost', function (err, db) {
+    if (!err) {
         console.log("connected to mongodb");
-        database = db;
+        GetMessages();
     }
 })
 
-var server = app.listen(5000, function(){
+//create  a server
+var server = app.listen(5000, function () {
     console.log("Listening on port", server.address().port);
 })
