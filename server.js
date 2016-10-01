e7a1fba4424a142e5b3e459b2762cd40fb1d074a
leighdinaya <leighdinaya04@gmail.com>
2016-10-01 10:08:37 -0700

    added mongodb modules

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongo = require('mongodb').MongoClient;
 
+var database;
+
 app.use(bodyParser.json());
 app.use(function(req,res,next){
     res.header("Access-Control-Allow-Origin", "*");
@@ -12,7 +14,15 @@ app.use(function(req,res,next){
 
 app.post('/api/message', function(req,res){
     console.log(req.body);
+     database.collection("messages").insertOne(req.body);
     res.status(200);
+})
+
+mongo.connect('mongodb://localhost:27017/mypost', function(err, db){
+    if(!err){
+        console.log("connected to mongodb");
+        database = db;
+    }
 })
 
 var server = app.listen(5000, function(){
