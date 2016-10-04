366ef36ec4bbcc437f1f2415c34e652f3561ed4e
leighdinaya <leighdinaya04@gmail.com>
2016-10-04 11:41:30 -0700

    added checking for existing email ad

---
+++ controllers/message.js
@@ -0,0 +1,18 @@
+var Message = require('../models/message');
+
+module.exports = {
+    //function to retrieve all the post/messages
+    get: function(req, res) {
+        //passing empty param
+        Message.find({}).exec(function (err, result) {
+            res.send(result);
+        });
+    },
+
+    post: function(req, res) {
+        console.log(req.body);
+        var message = new Message(req.body);
+        message.save();
+        res.status(200);
+    }
+}
--- server.js
+++ server.js
@@ -2,9 +2,8 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-
-var Message = require('./models/message');
-var User = require('./models/user');
+var auth = require('./controllers/auth');
+var message = require('./controllers/message');
 
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
@@ -14,28 +13,13 @@ app.use(function (req, res, next) {
 });
 
 //get
-app.get('/api/message', GetMessages);
+app.get('/api/message', message.get);
 
 //creat a post method
-app.post('/api/message', function (req, res) {
-    console.log(req.body);
-    var message = new Message(req.body);
-    message.save();
-    res.status(200);
-})
+app.post('/api/message', message.post);
 
 //post method for authentication
-app.post('/auth/register', function(req,res){
-    console.log(req.body)
-});
-
-//function to retrieve all the post/messages
-function GetMessages(req, res){
-    //passing empty param
-    Message.find({}).exec(function(err,result){
-        res.send(result);
-    });
-}
+app.post('/auth/register', auth.register);
 
 //connect to mongodb
 mongoose.connect('mongodb://localhost:27017/mypost', function (err, db) {
